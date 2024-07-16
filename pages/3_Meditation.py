557a8d7ffee754ce63d02d9bb26d48ddf8c53178
Aurelio <aureliopnf@gmail.com>
2024-07-15 21:48:41 -0300

    deploy: 0.1 version, contains basic meditation over time calculation

--- pages/3_Meditation.py
+++ pages/3_Meditation.py
@@ -1,40 +1,32 @@
 import streamlit as st
 
+# Debugging: print the list of selected characters
+#st.write("Selected Characters:", st.session_state["selected_characters"])
+
 st.write("# Meditação")
 
 # Slider for selecting a value from 1 to 30
-meditation_time = st.slider("Select meditation time (1-30)", min_value=1, max_value=30, step=1)
-
-# Example list of selected characters (you should replace this with st.session_state["selected_characters"])
-selected_characters = [
-    {
-        "1": {
-            "char_name": "Arashi",
-            "pph_base": 1.3200000000000003
-        }
-    },
-    {
-        "2": {
-            "char_name": "Hanzo",
-            "pph_base": 2.3499999999999996
-        }
-    }
-]
+meditation_time = st.slider("Selecione o tempo de meditação em dias(1-30 dias)", min_value=1, max_value=30, step=1)
+meditation_time_per_day = st.slider("Selecione o tempo de meditação por dia (1-20h)", min_value=1, max_value=20, step=1)
 
 # Predefined bonuses dictionary with fixed attributes
 extra_bonuses = {
-    "bonus_1": {"bonus_value": 5, "duration": 10},
-    "bonus_2": {"bonus_value": 3, "duration": 20},
-    "bonus_3": {"bonus_value": 7, "duration": 15}
+    "Pílula de Cultivo (Fraca)": 0.10,
+    "Pílula de Cultivo (Média)": 0.25,
+    "Plílula de Cultivo (Forte)": 0.5,
+    "Incenso de Meditação": 0.05,
+    "Incenso de Meditação Potente":0.15
 }
+
+selected_characters = st.session_state["selected_characters"]
 
 # Initialize session state for extra bonuses count if it doesn't exist
 if "bonus_counts" not in st.session_state:
-    st.session_state["bonus_counts"] = {char_id: {key: 0 for key in extra_bonuses} for char in selected_characters for char_id in char}
+    st.session_state["bonus_counts"] = {str(char_id): {key: 0 for key in extra_bonuses} for char in selected_characters for char_id in char}
 
 # Function to update bonus counts
 def update_bonus_counts(char_id, key, count):
-    st.session_state["bonus_counts"][char_id][key] = count
+    st.session_state["bonus_counts"][str(char_id)][key] = count
 
 # Display each character in a box with their name, PPH, and integer inputs for bonus counts
 for char in selected_characters:
@@ -54,21 +46,52 @@ for char in selected_characters:
             # Display integer inputs for each key in extra bonuses
             st.markdown("<hr class='solid'>", unsafe_allow_html=True)
             for key in extra_bonuses:
-                bonus_value = extra_bonuses[key]["bonus_value"]
-                duration = extra_bonuses[key]["duration"]
-                count = st.session_state["bonus_counts"][char_id].get(key, 0)
+                bonus_value = extra_bonuses[key]
+                count = st.session_state["bonus_counts"][str(char_id)].get(key, 0)
 
-                col1, col2 = st.columns([2, 1])
+                col1, col2 = st.columns([3, 1])
                 with col1:
-                    new_count = st.number_input("Quantidade", label_visibility="hidden" , min_value=0, max_value=99, value=count, step=1, key=f"{char_id}_{key}")
+                    new_count = st.number_input("Quantidade de Horas com esse bônus", min_value=0, max_value=99, value=count, step=1, key=f"{char_id}_{key}")
                     update_bonus_counts(char_id, key, new_count)
                 with col2:
                     st.write(f"{key}")
-                    st.write(f"Valor: {bonus_value}")
-                    st.write(f"Duração: {duration})")
+                    st.write(f"Valor (por hora): {bonus_value}")
                 st.markdown("<hr class='solid'>", unsafe_allow_html=True)
             
             st.markdown("</div>", unsafe_allow_html=True)
 
 # Debugging: print the list of selected characters with their bonus counts
-st.write("Updated Selected Characters with Bonus Counts:", st.session_state["bonus_counts"])
+# st.write("Updated Selected Characters with Bonus Counts:", st.session_state["bonus_counts"])
+
+
+st.write("Simulação de Meditação")
+
+for char in selected_characters:
+    for char_id, char_info in char.items():
+        char_name = char_info["char_name"]
+        pph_base = char_info["pph_base"]
+        total_meditation_time = meditation_time_per_day*meditation_time
+        total_meditation_value = pph_base*total_meditation_time
+        character_meditation_bonuses = st.session_state["bonus_counts"][str(char_id)]
+        # Iterate over the keys in st.session_state["bonus_counts"][str(char_id)]
+        for bonus in character_meditation_bonuses:
+            duration = character_meditation_bonuses[bonus]
+            if duration<= total_meditation_time:
+                total_bonus = duration*extra_bonuses[bonus]
+            else:
+                total_bonus = duration*total_meditation_time
+
+            total_meditation_value+= total_bonus
+
+        
+        with st.container():
+            st.markdown(f"""
+                        <div style="border: 1px solid #ddd; padding: 10px; margin: 10px 0; border-radius: 5px;">
+                            <div style="display: flex; justify-content: space-between;">
+                                <div style="font-size: 20px; font-weight: bold;">{char_name}</div>
+                                <div style="font-size: 16px;">PPH: {pph_base:.2f}</div>
+                            </div>
+                            <div style="font-size: 16px;"> Quantidade total de horas de meditação: {total_meditation_time:.0f} {"hora" if total_meditation_time==1 else "horas"}</div>
+                            <div style="font-size: 16px;"> Resultado da sessão de meditação: {total_meditation_value:.2f} Pontos</div>
+                        </div>
+                    """, unsafe_allow_html=True)
